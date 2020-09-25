54a0eec5d6246ac6d7b02a2a161f097056cb6bbd
David Lin <woei66@gmail.com>
2020-09-25 12:54:28 +0800

    fix

--- admin/hostname-add1.php
+++ admin/hostname-add1.php
@@ -27,7 +27,6 @@ try {
     $db->beginTransaction();
 
     $ttl = !empty($ttl) ? $ttl : 60;
-    $mx_priority = !empty($mx_priority) ? $mx_priority : 0;
     $refresh = 600;
     $expire = 86400;
     $minimum = 3600;
@@ -47,22 +46,36 @@ try {
     //$primary_ns = "dns.google";
 
     //$sql = "insert into dns_records (zone,host,type,data,ttl,mx_priority,refresh,retry,expire,minimum,serial,resp_person,primary_ns) values (:zone,:host,:type,:data,:ttl,:mx_priority,:refresh,:retry,:expire,:minimum,:serial,:resp_person,:primary_ns)";
-    $sql = "insert into dns_records (zone,host,type,data,ttl,mx_priority,serial) values (:zone,:host,:type,:data,:ttl,:mx_priority,:serial)";
-    $st = $db->prepare($sql);
-    $st->bindParam(':zone', $zone, PDO::PARAM_STR); // 域名
-    $st->bindParam(':host', $host, PDO::PARAM_STR); // 記錄名稱
-    $st->bindParam(':type', $type, PDO::PARAM_STR); // 記錄類型
-    $st->bindParam(':data', $data, PDO::PARAM_STR); // 記錄值
-    $st->bindParam(':ttl', $ttl, PDO::PARAM_STR); // ttl(存活時間)
-    $st->bindParam(':mx_priority', $mx_priority, PDO::PARAM_STR); // mx優先級
+    if (empty($mx_priority)) {
+        $sql = "insert into dns_records (zone,host,type,data,ttl,serial) values (:zone,:host,:type,:data,:ttl,:serial)";
+        $st = $db->prepare($sql);
+        $st->bindParam(':zone', $zone, PDO::PARAM_STR); // 域名
+        $st->bindParam(':host', $host, PDO::PARAM_STR); // 記錄名稱
+        $st->bindParam(':type', $type, PDO::PARAM_STR); // 記錄類型
+        $st->bindParam(':data', $data, PDO::PARAM_STR); // 記錄值
+        $st->bindParam(':ttl', $ttl, PDO::PARAM_STR); // ttl(存活時間)
+        $st->bindParam(':serial', $serial, PDO::PARAM_STR); // 序列號,每次更改配置都會在原來的基礎上加1
+        $st->execute();
+    } else {
+        $sql = "insert into dns_records (zone,host,type,data,ttl,mx_priority,serial) values (:zone,:host,:type,:data,:ttl,:mx_priority,:serial)";
+        $st = $db->prepare($sql);
+        $st->bindParam(':zone', $zone, PDO::PARAM_STR); // 域名
+        $st->bindParam(':host', $host, PDO::PARAM_STR); // 記錄名稱
+        $st->bindParam(':type', $type, PDO::PARAM_STR); // 記錄類型
+        $st->bindParam(':data', $data, PDO::PARAM_STR); // 記錄值
+        $st->bindParam(':ttl', $ttl, PDO::PARAM_STR); // ttl(存活時間)
+        $st->bindParam(':mx_priority', $mx_priority, PDO::PARAM_STR); // mx優先級
+        $st->bindParam(':serial', $serial, PDO::PARAM_STR); // 序列號,每次更改配置都會在原來的基礎上加1
+        $st->execute();
+    }
+
     //$st->bindParam(':refresh', $refresh, PDO::PARAM_STR); // 刷新時間間隔
     // $st->bindParam(':retry', $retry, PDO::PARAM_STR); // 重試時間間隔
     //$st->bindParam(':expire', $expire, PDO::PARAM_STR); // 過期時間
     //$st->bindParam(':minimum', $minimum, PDO::PARAM_STR); // 最小時間
-    $st->bindParam(':serial', $serial, PDO::PARAM_STR); // 序列號,每次更改配置都會在原來的基礎上加1
+
     //$st->bindParam(':resp_person', $resp_person, PDO::PARAM_STR); // 責任人
     //$st->bindParam(':primary_ns', $primary_ns, PDO::PARAM_STR); // 主域名
-    $st->execute();
 
     $id = $db->lastInsertId();
 
